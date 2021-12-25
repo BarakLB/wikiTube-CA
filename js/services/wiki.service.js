9893b9d8e16bcf3cd40222fd03921026ddff6f72
Barak Lebowitz <88040889+BarakLB@users.noreply.github.com>
2021-12-25 12:32:20 +0200

    Add files via upload

---
+++ js/services/wiki.service.js
@@ -0,0 +1,33 @@
+'use strict'
+
+
+
+function getVideos(value = '') {
+    if (!loadFromStorage(value) || !loadFromStorage(value).length) {
+        console.log('from API');
+
+        const prm = axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=AIzaSyASTnAMNooSJJ-NEW8yCpjnj7rv0VWKIX8&q=${value}`)
+            .then(res => {
+                saveToStorage(value, res.data.items);
+                return res.data.items
+            })
+        return prm;
+    }
+    console.log('From Cache');
+    const res = loadFromStorage(value);
+    return Promise.resolve(res)
+
+}
+
+
+
+
+function getWiki(value) {
+    // if (!value) return
+    return axios.get(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&srsearch=${value}&format=json`)
+        .then(res => res.data)
+        .catch(err => {
+            console.log('Cannot get ans', err);
+            throw err
+        })
+}
